3d64e07a1393a27dea3eb68e8f778c54904cbe9d
LeonPierre <lepierre08@gmail.com>
2021-07-05 10:18:06 -0400

    Add project files.

---
+++ com.LeonPierre.CodingInterview/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(com.LeonPierre.CodingInterview.Startup))]
+namespace com.LeonPierre.CodingInterview
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
